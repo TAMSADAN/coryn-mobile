3fe41ce496f49d8819071998a8fffa80253671ba
송영모 <mooyoung2309@gmail.com>
2021-12-29 16:43:59 +0900

    chore: edit ad script

--- mobile/lib/utils/ad_helper.dart
+++ mobile/lib/utils/ad_helper.dart
@@ -1,24 +1,26 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 
-const Map<String, String> UNIT_ID = kReleaseMode
-    ? {
-        'ios': '[YOUR iOS AD UNIT ID]',
-        'android': '[YOUR ANDROID AD UNIT ID]',
-      }
-    : {
-        'ios': 'ca-app-pub-3940256099942544/2934735716',
-        'android': 'ca-app-pub-3940256099942544/6300978111',
-      };
-
 class AdHelper {
   static String get bannerAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
-    } else {
-      throw new UnsupportedError('Unsupported platform');
+    if (kReleaseMode) {
+      if (Platform.isAndroid) {
+        return 'ca-app-pub-2392187154020666/5495255058';
+      } else if (Platform.isIOS) {
+        return 'ca-app-pub-2392187154020666/3753882950';
+      } else {
+        throw new UnsupportedError('Unsupported platform');
+      }
+    }
+    // 테스트
+    else {
+      if (Platform.isAndroid) {
+        return 'ca-app-pub-3940256099942544/6300978111';
+      } else if (Platform.isIOS) {
+        return 'ca-app-pub-3940256099942544/2934735716';
+      } else {
+        throw new UnsupportedError('Unsupported platform');
+      }
     }
   }
 
